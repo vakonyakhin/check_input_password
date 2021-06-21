591634efd37fd56941b01dc47d62a96e6ae4d68e
vakonyakhin <36918300+vakonyakhin@users.noreply.github.com>
2021-06-21 13:34:20 +0300

    Format code to PEP8 requirements
    Format code to PEP8 requirements

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import urwid
 
 def check_length_pass(text):
   return len(text) > 12
-     
+
 
 def has_digit(text):
   return any(letter.isdigit() for letter in text)
@@ -24,14 +24,13 @@ def has_upper(text):
 def has_symbol(text):
   return not text.isalnum()
 
- 
+
 
 def check_pass_value(text, list):
   value = 0
   for func in list:
     if func(text):
-      value += 2
-      
+      value += 2    
   return value
 
 
@@ -42,18 +41,18 @@ def main():
   menu = urwid.Pile([ask, reply])
   menu = urwid.Filler(menu, valign='top')
   functions = [
-              has_upper, 
-              has_lower, 
-              has_digit,
-              has_symbol,
-              check_length_pass,
-              has_alpha
-              ]
+      has_upper, 
+      has_lower, 
+      has_digit,
+      has_symbol,
+      check_length_pass,
+      has_alpha
+  ]
 
   def on_ask_change(edit, new_edit_text):
     reply.set_text("Рейтинг этого пароля: %s" % check_pass_value(new_edit_text, functions))
 
-  
+
   urwid.connect_signal(ask, 'change', on_ask_change)
   urwid.MainLoop(menu).run()
 if __name__ == "__main__":
